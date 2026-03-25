8aae374ea02e1363631b26da65c10aca0e0fa4e5
George Octavius <113178814+georgetheoracle@users.noreply.github.com>
2026-03-25 10:12:31 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div
+    class="bg-[#E7EDE7] w-full h-screen flex justify-center items-start pt-15">
+    <Card />
+  </div>
+</template>
+
+<script setup>
+import Card from './components/Card.vue';
+</script>
---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-white p-4 w-1/2 rounded-lg shadow-md">
+    <div
+      class="flex flex-col items-center bg-[#E7EDE7] w-6 py-2 space-y-0.5 rounded">
+      <button @click="count++" class="font-medium text-grey-100">+</button>
+      <span class="font-bold text-blue-500">{{ count }}</span>
+      <button @click="count--" class="font-medium text-grey-100">-</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+</script>
