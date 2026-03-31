e5439924d95d2b02a4fcfb63167ab5bd236a4013
George Octavius <113178814+georgetheoracle@users.noreply.github.com>
2026-03-30 23:15:14 -0400

    update

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,15 @@
-<template>
-  <div class="bg-[#E7EDE7] w-full min-h-screen">
-    <div class="flex flex-col gap-4 w-1/2 mx-auto py-4">
-      <!-- COMMENTS -->
-      <Card
-        v-for="comment in allComments"
-        :key="comment.id"
-        :user="comment"
-        :isReply="false"
-        @delete="deleteComment"
-        @edit="editComment"
-        @reply="addReply" />
-      <!-- REPLIES (BOTTOM) -->
-      <Card
-        v-for="(reply, index) in allReplies"
-        :key="reply.id"
-        :user="reply"
-        :isReply="true"
-        :replyIndex="index"
-        @delete="deleteComment"
-        @edit="editComment"
-        @reply="addReply" />
-      <!-- INPUT -->
-      <CommentBox @add-comment="addComment" />
-    </div>
-  </div>
-</template>
-
 <script setup>
 import Card from './components/Card.vue';
 import CommentBox from './components/CommentBox.vue';
 import { ref, onMounted, computed } from 'vue';
 import { formatDistanceToNowStrict } from 'date-fns';
 
-const user = ref([]);
+const comments = ref([]);
 
 const fetchComments = async () => {
   const res = await fetch('http://localhost:4000/comments');
   const data = await res.json();
-  user.value = data.map((c) => ({
+  comments.value = data.map((c) => ({
     ...c,
     time: formatTime(c.created_at),
   }));
@@ -50,14 +22,6 @@ const allComments = computed(() =>
 const allReplies = computed((parentId) =>
   user.value.filter((u) => u.parent_id === parentId),
 );
-// const getReplies = (id) => {
-//   return user.value
-//     .filter((u) => u.parent_id === id)
-//     .map((r) => ({
-//       ...r,
-//       time: formatTime(r.created_at),
-//     }));
-// };
 
 // ADD COMMENT
 const addComment = async (data) => {
@@ -120,20 +84,6 @@ const deleteComment = async (id) => {
 };
 
 // HELPERS
-function getCurrentUser() {
-  let user = localStorage.getItem('user');
-
-  if (!user) {
-    const randomId = Math.floor(Math.random() * 1000);
-    user = {
-      name: `User${randomId}`,
-      avatar: `https://i.pravatar.cc/150?img=${randomId}`,
-    };
-    localStorage.setItem('user', JSON.stringify(user));
-  }
-
-  return JSON.parse(user);
-}
 
 const formatTime = (date) => {
   const seconds = (new Date() - new Date(date)) / 1000;
@@ -144,3 +94,31 @@ const formatTime = (date) => {
   });
 };
 </script>
+
+<template>
+  <div class="bg-[#E7EDE7] w-full min-h-screen">
+    <div class="flex flex-col gap-4 w-1/2 mx-auto py-4">
+      <!-- COMMENTS -->
+      <Card
+        v-for="comment in allComments"
+        :key="comment.id"
+        :user="comment"
+        :isReply="false"
+        @delete="deleteComment"
+        @edit="editComment"
+        @reply="addReply" />
+      <!-- REPLIES (BOTTOM) -->
+      <!-- <Card
+        v-for="(reply, index) in allReplies"
+        :key="reply.id"
+        :user="reply"
+        :isReply="true"
+        :replyIndex="index"
+        @delete="deleteComment"
+        @edit="editComment"
+        @reply="addReply" /> -->
+      <!-- INPUT -->
+      <CommentBox @add-comment="addComment" />
+    </div>
+  </div>
+</template>
