cea1dec3011c1aeaedc27def58c420d71bfd4ea4
George Octavius <113178814+georgetheoracle@users.noreply.github.com>
2026-04-07 01:49:27 -0400

    Update

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -7,10 +7,10 @@ const handleDelete = () => {
 </script>
 <template>
   <div class="w-full h-screen bg-stone-900/50 top-0 fixed z-10">
-    <div class="p-20 w-75 mx-auto my-25 bg-white rounded-lg">
+    <div class="p-20 w-100 mx-auto my-25 bg-white rounded-lg">
       <h2 class="text-2xl font-bold mb-4">Confirm Deletion</h2>
       <p class="mb-6">Are you sure you want to delete this comment?</p>
-      <div class="flex justify-end gap-4">
+      <div class="flex items-center justify-center gap-4">
         <button
           @click="$emit('cancel')"
           class="px-4 py-2 rounded bg-gray-300 hover:bg-gray-400">
